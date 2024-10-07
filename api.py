d8c1f4d4c6b1e8418da3150ce869f2129d6fc67b
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2024-10-07 16:21:00 +0300

    Initial DLG gui added

--- api.py
+++ api.py
@@ -52,34 +52,49 @@ def login(email=None, password=None):
         return None
     print(password)
     print(email)
-    # resp = call_api('/auth', method='post', payload={"email": email, "password": password})
-    resp = requests.post(API_URL+ "/login/", json={"email": email, "password": password})
-    
-    if resp.status_code == 200:
-        print("Access Token:\t{}".format(resp.json().get("token")))
-        QtCore.QSettings().setValue("MISLANDAFRICA/email", email)
-        QtCore.QSettings().setValue("MISLANDAFRICA/password", password)
-        QtCore.QSettings().setValue("MISLANDAFRICA/token", resp.json().get("token"))
-        return resp
+    try:
+        # resp = call_api('/auth', method='post', payload={"email": email, "password": password})
+        resp = requests.post(API_URL+ "/login/", json={"email": email, "password": password})
         
-    elif str(resp.status_code).startswith("5"):
-        log("Could not connect to the MISLANDAFRICA server due to a server error. Error code: {}".format(resp.status_code))
+        if resp.status_code == 200:
+            print("Access Token:\t{}".format(resp.json().get("token")))
+            QtCore.QSettings().setValue("MISLANDAFRICA/email", email)
+            QtCore.QSettings().setValue("MISLANDAFRICA/password", password)
+            QtCore.QSettings().setValue("MISLANDAFRICA/token", resp.json().get("token"))
+            return resp
+            
+        elif str(resp.status_code).startswith("5"):
+            log("Could not connect to the MISLANDAFRICA server due to a server error. Error code: {}".format(resp.status_code))
+            QtWidgets.QMessageBox.critical(None,
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", "Could not connect to the MISLAND-AFRICA server due to a server error. Error code: {}".format(resp.status_code)))
+            return None
+        elif resp.json().get('non_field_errors') is not None:
+            log(resp.json().get('non_field_errors')[0])
+            QtWidgets.QMessageBox.critical(None,
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", resp.json().get('non_field_errors')[0]))
+            return None
+        else:
+            log("Could not login. Error code: {}".format(resp.status_code))
+            QtWidgets.QMessageBox.critical(None,
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
+                                    QtWidgets.QApplication.translate("MISLANDAFRICA", "Could not login. Error code: {}".format(resp.status_code)))
+            return None
+    
+    except requests.exceptions.ConnectionError:
+        log('API unable to access server - check internet connection')
         QtWidgets.QMessageBox.critical(None,
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", "Could not connect to the MISLAND-AFRICA server due to a server error. Error code: {}".format(resp.status_code)))
-        return None
-    elif resp.json().get('non_field_errors') is not None:
-        log(resp.json().get('non_field_errors')[0])
+                                    QtWidgets.QApplication.translate("MISLAND", "Error"),
+                                    QtWidgets.QApplication.translate("MISLAND", u"Unable to login to MISLAND-AFRICA server. Check your internet connection."))
+        resp = None
+    except requests.exceptions.Timeout:
+        log('API unable to login - general error')
         QtWidgets.QMessageBox.critical(None,
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", resp.json().get('non_field_errors')[0]))
-        return None
-    else:
-        log("Could not login. Error code: {}".format(resp.status_code))
-        QtWidgets.QMessageBox.critical(None,
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", "Error"),
-                                   QtWidgets.QApplication.translate("MISLANDAFRICA", "Could not login. Error code: {}".format(resp.status_code)))
-        return None
+                                    QtWidgets.QApplication.translate("MISLAND", "Error"),
+                                    QtWidgets.QApplication.translate("MISLAND", u"Unable to connect to the MISLAND-AFRICA server."))
+        resp = None
+        
         
 
 
