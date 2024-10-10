a005692968ffc3f51cc824fad70febc95f31f5d6
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2024-10-10 11:57:17 +0300

    Medalus complete

---
+++ data/computation_years.py
@@ -0,0 +1,419 @@
+computationYears = [
+    {
+        "computation_type": "Productivity State",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "SOC",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "Coastal Vulnerability Index",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "ILSWE",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "Forest Change",
+        "admin_zero": None,
+        "published_years": [
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "Forest Carbon Emission",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+        ]
+    },
+    {
+        "computation_type": "Forest Fire Risk",
+        "admin_zero": None,
+        "published_years": []
+    },
+    {
+        "computation_type": "Soil Quality Index",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "Vegetation Quality Index",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "Productivity State",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "Productivity Trajectory",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+        ]
+    },
+    {
+        "computation_type": "Management Quality Index",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "Climate Quality Index",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "Productivity Performance",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "Productivity",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "Land Degradation",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    {
+        "computation_type": "ESAI",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+        ]
+    },
+    {
+        "computation_type": "Forest Fire",
+        "admin_zero": None,
+        "published_years": []
+    },
+    {
+        "computation_type": "RUSLE",
+        "admin_zero": None,
+        "published_years": [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "LULC Change",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+    {
+        "computation_type": "LULC",
+        "admin_zero": None,
+        "published_years": [
+            2000,
+            2001,
+            2002,
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    }
+]
--- medalus.py
+++ medalus.py
@@ -13,6 +13,7 @@ class DlgMedalus(QtWidgets.QDialog, Ui_DlgMedalus):
         self.setupUi(self)
         self.qualityIndices = ["Climate","Vegetation","Soil","Management","Environmental Sensitive Areas"]
         self.computation = "Climate Quality Index"
+        self.api = '/climatequality/'
         self.IndexComboBox.addItems(self.qualityIndices)
         self.countries = [country['name_0'] for country in get_Adm0() if get_Adm0() is not None]
         self.computationYears = fetchComputationYears(self.computation)
@@ -26,32 +27,38 @@ class DlgMedalus(QtWidgets.QDialog, Ui_DlgMedalus):
         self.RegionComboBox.currentTextChanged.connect(self.getSubRegions)
         self.OutputPushButton.clicked.connect(self.fillOutputDestination)
         self.IndexComboBox.currentTextChanged.connect(self.evaluateComputation)
+        self.SubmitPushButton.clicked.connect(self.compute)
         
     def evaluateComputation(self):
         if self.IndexComboBox.currentText() == "Climate":
             self.computation = "Climate Quality Index"
             self.computationYears = fetchComputationYears(self.computation)
             self.YearComboBox.clear()
+            self.api = '/climatequality/'
             self.YearComboBox.addItems(self.computationYears if self.computationYears is not None else [])
         elif self.IndexComboBox.currentText() == "Vegetation":
             self.computation = "Vegetation Quality Index"
             self.computationYears = fetchComputationYears(self.computation)
             self.YearComboBox.clear()
+            self.api = '/vegetationquality/'
             self.YearComboBox.addItems(self.computationYears if self.computationYears is not None else [])
         elif self.IndexComboBox.currentText() == "Soil":
             self.computation = "Soil Quality Index"
             self.computationYears = fetchComputationYears(self.computation)
             self.YearComboBox.clear()
+            self.api = '/soilquality/'
             self.YearComboBox.addItems(self.computationYears if self.computationYears is not None else [])
         elif self.IndexComboBox.currentText() == "Management":
             self.computation = "Management Quality Index"
             self.computationYears = fetchComputationYears(self.computation)
             self.YearComboBox.clear()
+            self.api = '/managementquality/'
             self.YearComboBox.addItems(self.computationYears if self.computationYears is not None else [])
         elif self.IndexComboBox.currentText() == "Environmental Sensitive Areas":
-            self.computation = "Soil Quality Index"
+            self.computation = "ESAI"
             self.computationYears = fetchComputationYears(self.computation)
             self.YearComboBox.clear()
+            self.api = '/esai/'
             self.YearComboBox.addItems(self.computationYears if self.computationYears is not None else [])
             
     def fillOutputDestination(self):
@@ -71,3 +78,83 @@ class DlgMedalus(QtWidgets.QDialog, Ui_DlgMedalus):
         self.subregions, self.region_id = populate_sub_region(self.RegionComboBox.currentText(),self.country_id)
         self.SubRegionComboBox.addItems(self.subregions if self.subregions is not None else [])
         self.SubRegionComboBox.setCurrentIndex(-1)
+    
+    def compute(self):
+        progress_dialog = QtWidgets.QProgressDialog("Processing...", "Cancel", 0, 100, self)
+        progress_dialog.setWindowTitle("Fetching Land Productivity Raster")
+        progress_dialog.setWindowModality(Qt.WindowModal)
+
+        progress_bar = QtWidgets.QProgressBar(progress_dialog)
+        progress_dialog.setBar(progress_bar)
+
+        progress_dialog.show()
+        QtWidgets.QApplication.processEvents()
+        path = self.api
+        
+        if not self.CountryComboBox.currentText():
+            highlightComboBox(self.CountryComboBox)
+            QtWidgets.QMessageBox.critical(None, self.tr("Error"),
+                                       self.tr("Please select a country"))
+            return
+        
+        if not self.OutputLineEdit.text():
+            highlightLineEdit(self.OutputLineEdit)
+            QtWidgets.QMessageBox.critical(None, self.tr("Error"),
+                                       self.tr("Please provide an output file path"))
+            return
+        
+        
+        
+        filePath = self.OutputLineEdit.text()
+        payload = {
+            "admin_0": self.country_id,
+            "computation_type":self.computation,
+            "raster_type":1,
+            "end_year":int(self.YearComboBox.currentText()),
+            "cached":self.CacheCheckBox.isChecked(),
+            "start_year":int(self.YearComboBox.currentText()),
+            "show_change":1,
+            "veg_index":self.IndexComboBox.currentText(),
+            "transform":"area",
+        }
+        country = self.CountryComboBox.currentText()
+        region = self.RegionComboBox.currentText()
+        subregion = self.SubRegionComboBox.currentText()
+        if country and not region and not subregion:
+            #get the country id
+            for item in countries:
+                if country == item['name_0']:
+                    countryID = item["id"]
+                    break
+            payload["vector"] = countryID
+            payload["admin_level"] = 0
+            
+        elif country and region and not subregion:
+            for item in regions:
+                if region == item['name_1']:
+                    regionID = item["id"]
+                    break
+            
+            payload["vector"] = regionID
+            payload["admin_level"] = 1
+
+        elif country and region and subregion:
+            for item in subregions:
+                if subregion == item['name_2']:
+                    subRegionID = item["id"]
+                    break
+            
+            payload["vector"] = subRegionID
+            payload["admin_level"] = 2
+        
+        # task = threading.Thread(target=fetchRaster, args = (path, payload, filePath, progress_dialog, progress_bar))
+        # task.start()
+        # with ThreadPoolExecutor(max_workers=1) as executor:
+        #     future = executor.submit(fetchRaster, path, payload, filePath, progress_dialog, progress_bar)
+        #     result = future.result()
+        #     print(result)
+        #     if result is not None:
+        #         self.close()
+        result = fetchRaster(path, payload, filePath, progress_dialog, progress_bar,self.computation)
+        if result is not None:
+            self.close()
